6d332732edf11ecb8a7f72d4db176e15d1ed5e3c
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 17:44:36 -0600

    Create interface

---
+++ FileTagger/Models/ApplicationSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace FileTagger.Models
+{
+    public class ApplicationSetting
+    {
+        public string SettingName { get; set; }
+
+        public object SettingValue { get; set; }
+    }
+}
