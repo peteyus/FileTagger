f1adbb3df3d77b7ea85d9a082e24cb8caa63eaf3
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 17:51:35 -0600

    Create service skeleton

---
+++ FileTagger/Services/AppSettingsService.cs
@@ -0,0 +1,19 @@
+﻿using FileTagger.Interfaces;
+using FileTagger.Models;
+using System.Collections.Generic;
+
+namespace FileTagger.Services
+{
+    public class AppSettingsService : IAppSettingsService
+    {
+        public IEnumerable<ApplicationSetting> ReadSettings()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveSettings(IEnumerable<ApplicationSetting> settings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FileTaggerUnitTests/Services/AppSettingsServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using FileTagger.Interfaces;
+using FileTagger.Services;
+using FileTaggerUnitTests.TestInfrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileTaggerUnitTests.Services
+{
+    [TestClass]
+    public class AppSettingsServiceTests : TestBase
+    {
+        private IAppSettingsService classUnderTest;
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+
+            this.classUnderTest = new AppSettingsService();
+        }
+    }
+}
