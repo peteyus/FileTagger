1abedf898573434d9898f0b3ceef2665886309c0
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 23:04:20 -0600

    Add ribbon, viewmodel and start styling

--- FileTagger/ViewModels/MainViewModel.cs
+++ FileTagger/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using FileTagger.Extensions;
 using FileTagger.Interfaces;
 using FileTagger.Resources;
+using FileTagger.ViewModels.Menu;
 using GalaSoft.MvvmLight;
 using System;
 
@@ -11,12 +12,15 @@ namespace FileTagger.ViewModels
         private readonly IAppSettingsService appSettingsService;
         private string workingPath;
 
-        public MainViewModel(IAppSettingsService appSettingsService)
+        public MainViewModel(
+            IAppSettingsService appSettingsService,
+            RibbonMenuViewModel ribbonMenuViewModel)
         {
             appSettingsService.CheckWhetherArgumentIsNull(nameof(appSettingsService));
+            ribbonMenuViewModel.CheckWhetherArgumentIsNull(nameof(ribbonMenuViewModel));
 
             this.appSettingsService = appSettingsService;
-
+            this.Ribbon = ribbonMenuViewModel;
             var workingDirectory = this.appSettingsService.GetSetting(Constants.WorkingDirectory);
             this.workingPath = workingDirectory?.SettingValue.ToString() ?? AppDomain.CurrentDomain.BaseDirectory;
         }
@@ -30,5 +34,7 @@ namespace FileTagger.ViewModels
                 this.RaisePropertyChanged(nameof(this.WorkingPath)); 
             } 
         }
+
+        public RibbonMenuViewModel Ribbon { get; }
     }
 }
---
+++ FileTagger/Views/Menu/Ribbon.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FileTagger.Views.Menu
+{
+    /// <summary>
+    /// Interaction logic for Ribbon.xaml
+    /// </summary>
+    public partial class Ribbon : UserControl
+    {
+        public Ribbon()
+        {
+            InitializeComponent();
+        }
+    }
+}
