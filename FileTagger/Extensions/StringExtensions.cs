09ff873a97ea4528a3c67de4b1fb96a4db7b558e
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 20:58:07 -0600

    Add argument null check extension on objects and strings

---
+++ FileTagger/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FileTagger.Extensions
+{
+    public static class StringExtensions
+    {
+        public static void CheckWhetherArgumentIsNull(this string self, string argumentName)
+        {
+            if (self == null || string.IsNullOrWhiteSpace(self))
+            {
+                throw new System.ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
