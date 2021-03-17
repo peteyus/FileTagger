9e2a035d7edd8aab7b56630a3a4f8dd2b795b088
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 18:34:11 -0600

    Finish ReadSettings tests

--- FileTaggerUnitTests/Services/AppSettingsServiceTests.cs
+++ FileTaggerUnitTests/Services/AppSettingsServiceTests.cs
@@ -5,6 +5,7 @@ using FileTaggerUnitTests.TestInfrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FileTaggerUnitTests.Services
 {
@@ -40,6 +41,61 @@ namespace FileTaggerUnitTests.Services
                 this.MockSerializationService.Verify(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()), 
                     Times.Never, 
                     "Should not have attempted to deserialize anything.");
+            }
+
+            [TestMethod]
+            public void PassesFileContentsToSerializationService()
+            {
+                // arrange
+                this.MockFileSystem.MockFile.Setup(file => file.Exists(It.IsAny<string>())).Returns(true);
+                string fileContents = "blah blah blah.";
+                this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns(fileContents);
+
+                // act
+                this.classUnderTest.ReadSettings();
+
+                // assert
+                this.MockSerializationService.Verify(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(fileContents),
+                    Times.Once,
+                    "Should have attempted to deserialize the results.");
+            }
+
+            [TestMethod]
+            public void DoesNotFailWhenDeserializationFails()
+            {
+                // arrange
+                this.MockFileSystem.MockFile.Setup(file => file.Exists(It.IsAny<string>())).Returns(true);
+                string fileContents = "blah blah blah.";
+                this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns(fileContents);
+                this.MockSerializationService.Setup(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()))
+                    .Throws(new System.Exception("Can't get out that way kid."))
+                    .Verifiable("Should have attempted to deserialize");
+
+                // act
+                this.classUnderTest.ReadSettings();
+
+                // assert -- no exceptions.
+                this.MockSerializationService.Verify();
+            }
+
+            [TestMethod]
+            public void ReturnsDeserializedSettings()
+            {
+                // arrange
+                this.MockFileSystem.MockFile.Setup(file => file.Exists(It.IsAny<string>())).Returns(true);
+                string fileContents = "blah blah blah.";
+                var expectedSetting = new ApplicationSetting { SettingName = "blah", SettingValue = 42 };
+                this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns(fileContents);
+                this.MockSerializationService.Setup(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()))
+                    .Returns(new List<ApplicationSetting>() { expectedSetting } )
+                    .Verifiable("Should have attempted to deserialize");
+
+                // act
+                var result = this.classUnderTest.ReadSettings();
+
+                // assert
+                this.MockSerializationService.Verify();
+                Assert.AreEqual(expectedSetting, result.First(), "Should have returned the expected setting.");
             }
         }
     }
