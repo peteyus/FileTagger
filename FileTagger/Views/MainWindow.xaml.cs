1b8b71fd8673b33a9ae74719bba8eeda64b4e730
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 22:22:21 -0600

    Update MainWindow to RibbonWindow control. Move to Views namespace.

---
+++ FileTagger/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace FileTagger.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
