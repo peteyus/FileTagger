9d6d1989ec108f2e1c9ed2fe4c66876af897d5bf
Peter Jorgensen <prjorgen@gmail.com>
2021-03-16 19:00:55 -0600

    Add more tests for AppSettingsService

--- FileTaggerUnitTests/Services/AppSettingsServiceTests.cs
+++ FileTaggerUnitTests/Services/AppSettingsServiceTests.cs
@@ -4,6 +4,7 @@ using FileTagger.Services;
 using FileTaggerUnitTests.TestInfrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,6 +23,18 @@ namespace FileTaggerUnitTests.Services
             this.classUnderTest = this.CreateAppSettingsService();
         }
 
+        private ApplicationSetting SetupTestToReturnApplicationSetting(string settingName, object settingValue)
+        {
+            var expectedSetting = new ApplicationSetting { SettingName = settingName, SettingValue = settingValue };
+
+            this.MockFileSystem.MockFile.Setup(file => file.Exists(It.IsAny<string>())).Returns(true);
+            this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns("all the settings");
+            this.MockSerializationService.Setup(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()))
+                .Returns(new List<ApplicationSetting>() { expectedSetting });
+
+            return expectedSetting;
+        }
+
         [TestClass]
         public class ReadSettingsTests : AppSettingsServiceTests
         {
@@ -38,8 +51,8 @@ namespace FileTaggerUnitTests.Services
                 this.MockFileSystem.MockFile.Verify(file => file.ReadAllText(It.IsAny<string>()),
                     Times.Never,
                     "Should not have attempted to read the nonexistant file.");
-                this.MockSerializationService.Verify(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()), 
-                    Times.Never, 
+                this.MockSerializationService.Verify(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()),
+                    Times.Never,
                     "Should not have attempted to deserialize anything.");
             }
 
@@ -68,7 +81,7 @@ namespace FileTaggerUnitTests.Services
                 string fileContents = "blah blah blah.";
                 this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns(fileContents);
                 this.MockSerializationService.Setup(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()))
-                    .Throws(new System.Exception("Can't get out that way kid."))
+                    .Throws(new Exception("Can't get out that way kid."))
                     .Verifiable("Should have attempted to deserialize");
 
                 // act
@@ -82,20 +95,120 @@ namespace FileTaggerUnitTests.Services
             public void ReturnsDeserializedSettings()
             {
                 // arrange
-                this.MockFileSystem.MockFile.Setup(file => file.Exists(It.IsAny<string>())).Returns(true);
-                string fileContents = "blah blah blah.";
-                var expectedSetting = new ApplicationSetting { SettingName = "blah", SettingValue = 42 };
-                this.MockFileSystem.MockFile.Setup(file => file.ReadAllText(It.IsAny<string>())).Returns(fileContents);
-                this.MockSerializationService.Setup(serialization => serialization.DeserializeObject<IEnumerable<ApplicationSetting>>(It.IsAny<string>()))
-                    .Returns(new List<ApplicationSetting>() { expectedSetting } )
-                    .Verifiable("Should have attempted to deserialize");
+                var expectedSetting = this.SetupTestToReturnApplicationSetting("blah", 42);
 
                 // act
                 var result = this.classUnderTest.ReadSettings();
 
                 // assert
-                this.MockSerializationService.Verify();
                 Assert.AreEqual(expectedSetting, result.First(), "Should have returned the expected setting.");
+            }
+        }
+
+        [TestClass]
+        public class SaveSettingsTests : AppSettingsServiceTests
+        {
+            [TestMethod]
+            public void ReturnsFalseWhenSerializationFails()
+            {
+                // arrange
+                this.MockSerializationService.Setup(serialization => serialization.SerializeObject(It.IsAny<object>()))
+                    .Throws(new Exception("I'm afraid I can't do that Dave."))
+                    .Verifiable("Should have attempted to serialize the settings.");
+
+                // act
+                var result = this.classUnderTest.SaveSettings();
+
+                // assert
+                Assert.IsFalse(result, "Should return false.");
+                this.MockSerializationService.Verify();
+            }
+
+            [TestMethod]
+            public void ReturnsFalseWhenSavingFileFails()
+            {
+                // arrange
+                this.MockSerializationService.Setup(serialization => serialization.SerializeObject(It.IsAny<object>()))
+                    .Returns("Totally serial.");
+                this.MockFileSystem.MockFile.Setup(file => file.WriteAllText(It.IsAny<string>(), It.IsAny<string>()))
+                    .Throws(new Exception("No. Just no."))
+                    .Verifiable("Should have attempted to save the file.");
+
+                // act
+                var result = this.classUnderTest.SaveSettings();
+
+                // assert
+                Assert.IsFalse(result, "Should return false.");
+                this.MockFileSystem.MockFile.Verify();
+            }
+
+            [TestMethod]
+            public void ReturnsTrueWhenNoErrors()
+            {
+                // arrange
+                this.MockSerializationService.Setup(serialization => serialization.SerializeObject(It.IsAny<object>()))
+                    .Returns("Totally serial.")
+                    .Verifiable("Should have attempted to serialize");
+                this.MockFileSystem.MockFile.Setup(file => file.WriteAllText(It.IsAny<string>(), It.IsAny<string>()))
+                    .Verifiable("Should have attempted to save the file.");
+
+                // act
+                var result = this.classUnderTest.SaveSettings();
+
+                // assert
+                Assert.IsTrue(result, "Should have succeeded.");
+                this.MockSerializationService.Verify();
+                this.MockFileSystem.MockFile.Verify();
+            }
+        }
+
+        [TestClass]
+        public class GetSettingValueTests : AppSettingsServiceTests
+        {
+            [TestMethod]
+            public void ThrowsExceptionWhenNameIsNull()
+            {
+                // arrange
+                try
+                {
+                    // act
+                    this.classUnderTest.GetSettingValue(null);
+                    Assert.Fail("Should have thrown an exception.");
+                }
+                catch (ArgumentNullException e)
+                {
+                    Assert.AreEqual("setting", e.ParamName, "Should have warned that the setting cannot be null.");
+                }
+            }
+
+            [TestMethod]
+            public void AttemptsToReadSettingsIfNameNotFound()
+            {
+                // arrange
+                this.MockFileSystem.MockFile.Reset();
+                this.SetupTestToReturnApplicationSetting("Bob", "boop");
+
+                // act
+                this.classUnderTest.GetSettingValue("Bob");
+
+                // assert
+                this.MockFileSystem.MockFile.Verify(file => file.Exists(It.IsAny<string>()),
+                    Times.Once,
+                    "Should have attempted to re-read the file.");
+            }
+
+            [TestMethod]
+            public void DoesNotRequireCaseSensitiveMatch()
+            {
+                // arrange
+                string something = "SoMeThInG";
+                var expectedSetting = this.SetupTestToReturnApplicationSetting(something, "Something else.");
+
+                // act
+                var result = this.classUnderTest.GetSettingValue(something.ToLower());
+
+                // assert
+                Assert.AreEqual(expectedSetting.SettingValue, result, "Should have matched even with the case not matching.");
             }
         }
     }
