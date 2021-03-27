713b34fd8f52b39b4502a405f18acfb8dd28f9a5
Peter Jorgensen <prjorgen@gmail.com>
2021-03-27 12:23:27 -0600

    Show filetype icon on files

--- FileTagger/Models/Nodes/NodeBase.cs
+++ FileTagger/Models/Nodes/NodeBase.cs
@@ -1,15 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace FileTagger.Models.Nodes
 {
     public abstract class NodeBase
     {
+        private ImageSource icon;
+
         public string Name { get; set; }
 
         public string FullPath { get; set; }
 
-        public string Icon { get; set; } // TODO PRJ: FontAwesome?
+        public ImageSource Icon
+        {
+            get
+            {
+                if (this.icon == null && File.Exists(this.FullPath))
+                {
+                    using (Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(this.FullPath))
+                    {
+                        this.icon = Imaging.CreateBitmapSourceFromHIcon(
+                                  sysicon.Handle,
+                                  System.Windows.Int32Rect.Empty,
+                                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                    }
+                }
+
+                return this.icon;
+            }
+        }
 
         public ICollection<NodeBase> ChildNodes { get; } = new ObservableCollection<NodeBase>();
 
