56e37d599659fa40c4a38e0307ce7b5e8e47166e
Peter Jorgensen <prjorgen@gmail.com>
2021-04-05 22:43:00 -0600

    Create CommandBase class

---
+++ FileTagger/Commands/AddTagsCommand.cs
@@ -0,0 +1,19 @@
+﻿using FileTagger.Models.Nodes;
+using System;
+using System.Windows.Input;
+
+namespace FileTagger.Commands
+{
+    public class AddTagsCommand : CommandBase
+    {   
+        public override bool CanExecute(object parameter)
+        {
+            return parameter is FileNode;
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileTagger/Commands/CommandBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FileTagger.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+
+            // TODO PRJ: Needed? Is the event enough?
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
